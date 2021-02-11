87ec5beda0fff64d3096d9dc0a4c7f199d37bd2a
xrude <it17139@glbitm.ac.in>
2021-02-11 17:56:38 +0530

    updated

---
+++ TypeScript/Second Program/variables.js
@@ -0,0 +1,8 @@
+var num = 5 + 5;
+console.log(num);
+var str = "Hello World";
+console.log(str);
+var bool = true;
+console.log(bool);
+var bool1 = false;
+console.log(bool1);
---
+++ TypeScript/Second Program/variables.ts
@@ -0,0 +1,12 @@
+var num:number = 5 + 5;
+console.log(num);
+
+
+var str:string = "Hello World";
+console.log(str);
+
+var bool:boolean = true;
+console.log(bool);
+
+var bool1: boolean = false;
+console.log(bool1);
